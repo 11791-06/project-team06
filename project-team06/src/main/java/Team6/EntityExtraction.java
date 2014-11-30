4f10bd7a0c92650806398121e61ca5d50c0f3ce2
Rahul Goutam <rahulgoutam07@gmail.com>
2014-11-30 12:10:06 -0500

    Entity Extraction from top 100 snippets done. Using lingpipe with HMMChunker for NER

---
+++ project-team06/src/main/java/Team6/EntityExtraction.java
@@ -0,0 +1,81 @@
+package Team6;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.uima.UIMARuntimeException;
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.FSIterator;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+
+import com.aliasi.chunk.Chunk;
+import com.aliasi.chunk.Chunker;
+import com.aliasi.util.AbstractExternalizable;
+
+import edu.cmu.lti.oaqa.type.answer.Answer;
+import edu.cmu.lti.oaqa.type.retrieval.Document;
+import edu.cmu.lti.oaqa.type.retrieval.Passage;
+
+public class EntityExtraction extends JCasAnnotator_ImplBase {
+  Chunker model;
+  
+  @Override
+  public void initialize(UimaContext aContext) throws ResourceInitializationException {
+    // TODO Auto-generated method stub
+    try {
+      File f = new File(this.getClass().getClassLoader().getResource((String)aContext.getConfigParameterValue("ModelName")).getFile());
+      model = (Chunker) AbstractExternalizable.readObject(f);
+    } catch(Exception e) {
+      throw new UIMARuntimeException(e);
+    }
+  }
+  
+  @Override
+  public void process(JCas aJCas) throws AnalysisEngineProcessException {
+    // TODO Auto-generated method stub
+    int ranklimit = 100; 
+    HashMap<String, Integer> entities = new HashMap<String, Integer>();
+    FSIterator iter = aJCas.getJFSIndexRepository().getAllIndexedFS(Passage.type);
+    while(iter.hasNext()) {
+      Passage snippet = (Passage) iter.next();
+      if(snippet.getRank() <= ranklimit) {
+        String text = snippet.getText();
+        Set<Chunk> namedEntities = model.chunk(text).chunkSet();
+        for(Chunk c : namedEntities) {
+          String entityName = text.substring(c.start(), c.end());
+          if(entities.containsKey(entityName)) {
+            entities.put(entityName,  entities.get(entityName) + 1);
+          }
+          else {
+            entities.put(entityName,  1);
+          }
+        }
+      }
+    }
+    List<String> entityNames = new ArrayList<String>();
+    entityNames.addAll(entities.keySet());
+    Collections.sort(entityNames,  new Comparator<String>() {
+      public int compare(String o1, String o2) {
+        return entities.get(o1).compareTo(entities.get(o2));
+      }
+    });
+    
+    Collections.reverse(entityNames);
+    
+    int rank = 0;
+    for(String entityName : entityNames) {
+      Answer ans = new Answer(aJCas);
+      ans.setRank(++rank);
+      ans.setText(entityName);
+      //System.out.println("Entity : " + entityName + " rank : " + rank + " freq : " + entities.get(entityName));
+    }
+  }
+}
